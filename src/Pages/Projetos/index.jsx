f424bc12b857e2744823f5755389220cb049f8c5
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2022-12-19 19:49:22 -0300

    feat: sidebar on Projects page

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -2,8 +2,153 @@ import styles from './styles.module.css';
 
 export const Projetos = () => {
     return (
-        <>
-            <p>Tela de projetos</p>
-        </>
+        <div className={styles.container}>
+            <main>Main content</main>
+            <aside className={styles.sidebar}>
+                <div className={styles.card}>
+                    <div className={styles.preview}>
+                        <p className={styles.title}>Casa de praia</p>
+                    </div>
+
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/android/512/place-marker.png" alt="Localização" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/expand.png" alt="Tamanho" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/bedroom.png" alt="Quartos" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/external-victoruler-solid-victoruler/512/external-toilet-furniture-and-home-decor-vol1-victoruler-solid-victoruler.png" alt="Banheiros" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/car.png" alt="Estacionamento" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Recife - PE</td>
+                                <td>217.63m²</td>
+                                <td>4 <div>&#40;1 suíte&#41;</div></td>
+                                <td>3</td>
+                                <td>2</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className={styles.card}>
+                    <div className={styles.preview}>
+                        <p className={styles.title}>Casa de praia</p>
+                    </div>
+
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/android/512/place-marker.png" alt="Localização" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/expand.png" alt="Tamanho" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/bedroom.png" alt="Quartos" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/external-victoruler-solid-victoruler/512/external-toilet-furniture-and-home-decor-vol1-victoruler-solid-victoruler.png" alt="Banheiros" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/car.png" alt="Estacionamento" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Recife - PE</td>
+                                <td>217.63m²</td>
+                                <td>4</td>
+                                <td>3 <div>&#40;1 suíte&#41;</div></td>
+                                <td>2</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className={styles.card}>
+                    <div className={styles.preview}>
+                        <p className={styles.title}>Casa de praia</p>
+                    </div>
+
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/android/512/place-marker.png" alt="Localização" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/expand.png" alt="Tamanho" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/bedroom.png" alt="Quartos" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/external-victoruler-solid-victoruler/512/external-toilet-furniture-and-home-decor-vol1-victoruler-solid-victoruler.png" alt="Banheiros" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/car.png" alt="Estacionamento" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Recife - PE</td>
+                                <td>217.63m²</td>
+                                <td>4</td>
+                                <td>3 <div>&#40;1 suíte&#41;</div></td>
+                                <td>2</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className={styles.card}>
+                    <div className={styles.preview}>
+                        <p className={styles.title}>Casa de praia</p>
+                    </div>
+
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/android/512/place-marker.png" alt="Localização" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/expand.png" alt="Tamanho" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/bedroom.png" alt="Quartos" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/external-victoruler-solid-victoruler/512/external-toilet-furniture-and-home-decor-vol1-victoruler-solid-victoruler.png" alt="Banheiros" />
+                                </td>
+                                <td>
+                                    <img className={styles.icon} src="https://img.icons8.com/ios-glyphs/512/car.png" alt="Estacionamento" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Recife - PE</td>
+                                <td>217.63m²</td>
+                                <td>4</td>
+                                <td>3 <div>&#40;1 suíte&#41;</div></td>
+                                <td>2</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </aside>
+        </div>
     )
 }
