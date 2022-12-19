7a29637821859ef7737ff0e37bf209b3985a5368
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2022-12-19 12:28:00 -0300

    feat: Crossfade Gallery with bugs

---
+++ src/Components/GalleryPreview/index.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css';
+
+export const GalleryPreview = () => {
+    return (
+        <section>
+            <img src="https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Casa de praia" />
+            <img src="https://images.pexels.com/photos/2724749/pexels-photo-2724749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Cozinha" />
+            <img src="https://images.pexels.com/photos/1571470/pexels-photo-1571470.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Sala de espera" />
+            <img src="https://images.pexels.com/photos/7031737/pexels-photo-7031737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Sala de estar" />
+        </section>
+    )
+}
--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -1,8 +1,10 @@
+import { GalleryPreview } from '../../Components/GalleryPreview';
 import styles from './styles.module.css';
 
 export const Inicio = () => {
     return (
         <>
+            <GalleryPreview />
             <p>Tela inicial</p>
         </>
     )
